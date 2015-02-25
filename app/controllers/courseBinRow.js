761f9949863f5e5bdfb44d038cf4219a59a6709b
xzhang <xzhangcpp@gmail.com>
2015-02-24 18:27:46 -0800

    add swipe effect to course bin

--- app/controllers/courseBinRow.js
+++ app/controllers/courseBinRow.js
@@ -9,3 +9,16 @@ if($model){
 function remove(){
 	courseBin.remove($model);
 }
+function swipeLeft(e){
+	if (e.direction == "left") {//when swipe left
+		$.rowView.animate({
+			left : "-100dp"
+		});
+	}
+	else{
+		$.rowView.animate({
+			left : "0dp"
+		}); 
+	}
+
+}
--- app/controllers/courseList.js
+++ app/controllers/courseList.js
@@ -50,7 +50,8 @@ function updateTable(table,courses){
 	var data=[];
 	for(var i= 0; i < courses.length; i++){
 		var row = Titanium.UI.createTableViewRow({
-			title:courses[i].title()
+			title:courses[i].title(),
+			hasChild:true
 		});
 		data.push(row);
 	}
