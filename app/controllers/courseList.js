5bbfa137414560507113efc15ac20aea29a2b15b
Determ <jxyfwrj@gmail.com>
2015-02-27 16:16:28 -0800

    4:16
    colors

--- app/controllers/courseList.js
+++ app/controllers/courseList.js
@@ -56,7 +56,7 @@ function updateTable(table,courses){
 	for(var i= 0; i < courses.length; i++){
 		var row = Titanium.UI.createTableViewRow({
 			title:courses[i].title(),
-			backgroundColor: i%2 == 0?"#F7F7F7":"#D7D7D7", 
+			backgroundColor: i%2 == 0?"#FFF":"#D7D7D7", 
 			selectedBackgroundColor:"#FF3B30",
 			hasChild:true
 		});
